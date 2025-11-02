7ae765f1a705cd05b3d4d7d9e81916f4455faa88
AlenA109-1 <newmymail@yandex.ru>
2025-11-02 15:07:59 +0300

    lesson1

---
+++ lesson_1_task_5.py
@@ -0,0 +1,17 @@
+'''Создайте функцию, которая принимает на вход параметр num и печатает этот параметр в консоль.
+Вызовите эту функцию 11 раз так, чтобы в консоль вывелся номер 88005553535.'''
+
+def print_phone(num):
+    print(num, end = '')
+
+print_phone(8)
+print_phone(8)
+print_phone(0)
+print_phone(0)
+print_phone(5)
+print_phone(5)
+print_phone(5)
+print_phone(3)
+print_phone(5)
+print_phone(3)
+print_phone(5)
