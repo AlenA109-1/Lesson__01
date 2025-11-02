1d4111732c7c78ea9e6f35eb0690b16a5a7c3a2a
AlenA109-1 <newmymail@yandex.ru>
2025-11-02 18:48:08 +0300

    lesson1 task3

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -5,4 +5,4 @@
 
 first_name = input("Введите ваше имя_")
 last_name = input("Введите вашу фамилию_")
-print("Вас зовут:" + first_name + " " + last_name)
+print("Вас зовут:" + last_name + " " + first_name)
